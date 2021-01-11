33b62be09c6fb5c5bbb31f9f331497f1e83a41ab
ian.rosenberg <isr5@njit.edu>
2021-01-11 05:38:21 -0500

    Was missing the include folder

---
+++ include/graphics.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+#define FRAME_DELAY 16
+
+class Graphics {
+private:
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+
+	//Used for intialization of graphics, image, audio and text subsystems
+	int SDL2_Init(Uint8 flags, Uint16 windowWidth, Uint16 windowHeight);
+
+protected:
+	int32_t deltaTime;
+	Uint32 lastUpdateTime;
+	Uint32 currentUpdateTime;
+
+	float framerate;
+
+public:
+	Graphics();
+
+	~Graphics();
+
+	void NextFrame();
+
+	void FrameDelay();
+
+	void SetCurrentUpdateTime();
+
+	SDL_Renderer* GetRenderer();
+};
